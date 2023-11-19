786aac69906c1571528bbc995624091559d09341
Ridha Ilham Adi Setyawan <117886100+ridhailham@users.noreply.github.com>
2023-11-20 04:29:39 +0700

    create eksekusi basdat

---
+++ Eksekusi/Desain_Basdat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Eksekusi;
+
+
+public class Desain_Basdat {
+
+    public static void main(String[] args) {
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+    }
+}
