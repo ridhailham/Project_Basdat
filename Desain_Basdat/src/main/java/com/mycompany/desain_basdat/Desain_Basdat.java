f3f3053ef9640316ea8491f59ea43e4559f26232
Ridha Ilham Adi Setyawan <117886100+ridhailham@users.noreply.github.com>
2023-11-25 09:09:31 +0700

    create GUI

--- Desain_Basdat/src/main/java/com/mycompany/desain_basdat/Desain_Basdat.java
+++ Desain_Basdat/src/main/java/com/mycompany/desain_basdat/Desain_Basdat.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package Eksekusi;
+package com.mycompany.desain_basdat;
 
 
 public class Desain_Basdat {
